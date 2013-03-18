817bf7857b2952d609660b45865a0d1b3bf0c6b9
icro <icro@illi.be>
2013-03-18 13:25:25 +0100

    multi bootload support

--- illi.php
+++ illi.php
@@ -3,31 +3,41 @@
 	{
 		CLASS Init
 		{
-			const ENCODING        			= 'UTF-8';
+			const ENCODING		= 'UTF-8';
 			
-			private static $__isDebug		= FALSE;
-			private static $__isDevelopment		= FALSE;
+			CONST DO_SETUP		= 1;
+			CONST DO_SERVER		= 2;
+			CONST DO_DEV		= 4;
 			
-			public static $dir = NULL;
+			protected static $__map =
+			[
+				self::DO_SETUP	=> 'setup',
+				self::DO_SERVER	=> 'debug',
+				self::DO_DEV	=> 'development'
+			];
 			
-			static function setup()
+			static function run($__flag)
 			{
-				self::$dir = __DIR__;
+				foreach(static::$__map as $f => $m)
+					if($__flag & $f)
+						static::$m();
+			}
+			
+			protected static function setup()
+			{
 				date_default_timezone_set('Europe/Paris');
 			}
 			
-			static function debug()
+			protected static function debug()
 			{
-				self::$__isDebug = TRUE;
 				error_reporting(E_ALL | E_STRICT | E_DEPRECATED | E_USER_ERROR | E_USER_WARNING | E_USER_NOTICE);
 				ini_set("display_errors", 1);
 				ini_set("display_startup_errors", 1);
 				ini_set("error_log", "none");
 			}
 			
-			static function development()
+			protected static function development()
 			{
-				self::$__isDevelopment = TRUE;
 				clearstatcache();
 				header('Content-Type: text/plain; charset='.static::ENCODING);
 			}
@@ -38,16 +48,35 @@
 	{
 		CLASS Init
 		{
-			const ILLI = '3.2.0';
+			static $__init = [];
+			
+			static function add($__app, $__version, $__baseDir, array $__path = [])
+			{
+				static::$__init[$__app] = function() use ($__version, $__baseDir, $__path)
+				{
+					[] !== $__path ?: $__path = ['config', 'bootstrap.php'];
+					$path = implode(DIRECTORY_SEPARATOR, array_merge([$__baseDir, $__version, 'framework'], $__path));
+					require_once $path;
+				};
+			}
+			
+			static function run()
+			{
+				foreach(static::$__init as $app => $load)
+				{
+					$load();
+					unset(static::$__init[$app]);
+				}
+			}
+			
+			static function remove($__app)
+			{
+				unset(static::$__init[$__app]);
+			}
 			
 			static function illi()
 			{
-				$_ = function($_)
-				{
-					return implode(DIRECTORY_SEPARATOR, array_merge([dirname(__FILE__), static::ILLI, 'framework'], $_));
-				};
-				
-				require_once $_(['config', 'bootstrap.php']);
+				static::add('ILLI', '3.2.0', __DIR__);
 			}
 		}
 	}
